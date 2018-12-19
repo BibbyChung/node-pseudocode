f1759dc7c54bbba0e70a010818588377afad7394
Bibby <bibbynet@gmail.com>
2018-12-19 15:37:55 +0800

    add examples

---
+++ code/src/appHelper.ts
@@ -0,0 +1,38 @@
+import { from } from 'rxjs';
+import { groupBy, mergeMap, reduce, toArray, bufferCount } from 'rxjs/operators';
+
+export class AppHelper {
+  static async groupBy<T>(data: T[], func: (obj: T) => any) {
+    return new Promise((resolve, reject) => {
+      const arr = [];
+      from(data).pipe(
+        groupBy(func),
+        mergeMap<any, any>(group$ => group$.pipe(toArray())),
+        // mergeMap<any, any>(group$ => group$.pipe(
+        //   reduce((acc, cur) => [...acc, cur], [])
+        // ))
+      ).subscribe(obj => {
+        arr.push(obj);
+      }, (err) => {
+        reject(err);
+      }, () => {
+        resolve(arr);
+      });
+    });
+  }
+
+  static async chunk<T>(data: T[], chunkSize: number) {
+    return new Promise((resolve, reject) => {
+      const arr = [];
+      from(data).pipe(
+        bufferCount(chunkSize)
+      ).subscribe(a => {
+        arr.push(a);
+      }, (err) => {
+        reject(err);
+      }, () => {
+        resolve(arr);
+      });
+    });
+  }
+}
--- code/src/main.ts
+++ code/src/main.ts
@@ -1,4 +1,30 @@
-const fn = () => {
-  console.log('xxxx');
+import { AppHelper } from './appHelper';
+
+const fn = async () => {
+  const data = [
+    {
+      id: 1,
+      name: 'Bibby0'
+    },
+    {
+      id: 2,
+      name: 'Bibby1'
+    },
+    {
+      id: 3,
+      name: 'Bibby2'
+    },
+    {
+      id: 2,
+      name: 'Bibby3'
+    },
+  ];
+
+  const groupByArr = await AppHelper.groupBy(data, a => `${a.id}`);
+  console.log(groupByArr);
+
+  const chunkArr = await AppHelper.chunk(data, 3);
+  console.log(chunkArr);
 };
+
 fn();
